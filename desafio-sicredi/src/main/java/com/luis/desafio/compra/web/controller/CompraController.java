ac7e4a1e1f3d2ae76cb198ba4bb82a740c6872b5
luis antonio <tyluis20@gmail.com>
2024-10-17 13:06:10 -0300

    adicionar swagger

--- desafio-sicredi/src/main/java/com/luis/desafio/compra/web/controller/CompraController.java
+++ desafio-sicredi/src/main/java/com/luis/desafio/compra/web/controller/CompraController.java
@@ -3,6 +3,9 @@ package com.luis.desafio.compra.web.controller;
 import com.luis.desafio.compra.model.Compra;
 import com.luis.desafio.compra.service.CompraService;
 import com.luis.desafio.compra.web.dto.CadastrarCompraDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +29,12 @@ public class CompraController {
     }
 
     @PostMapping("/cadastrar")
+    @Operation(summary = "Cadastrar uma nova compra",
+            description = "Cadastra uma nova compra a partir dos dados fornecidos.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Compra cadastrada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro ao cadastrar a compra")
+    })
     public ResponseEntity<?> candidatarParaVaga(@RequestBody CadastrarCompraDTO cadastrarCompraDTO) {
         try {
             Compra compra = compraService.cadastrarCompra(cadastrarCompraDTO);
@@ -36,6 +45,12 @@ public class CompraController {
     }
 
     @GetMapping("/pesquisar")
+    @Operation(summary = "Pesquisar compras",
+            description = "Pesquisa compras com base nos filtros fornecidos.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Compras encontradas com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Nenhuma compra encontrada")
+    })
     public ResponseEntity<List<Compra>> pesquisarCompras(
             @RequestParam(required = false) String cpf,
             @RequestParam(required = false) String nomeProduto,
--- desafio-sicredi/src/main/java/com/luis/desafio/produto/controller/ProdutoController.java
+++ desafio-sicredi/src/main/java/com/luis/desafio/produto/controller/ProdutoController.java
@@ -1,7 +1,10 @@
 package com.luis.desafio.produto.controller;
 
+
 import com.luis.desafio.produto.dto.RelatorioProdutoDTO;
 import com.luis.desafio.produto.service.ProdutoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +23,11 @@ public class ProdutoController {
     private ProdutoService produtoService;
 
     @GetMapping("/relatorio")
+    @Operation(summary = "Gera um relatório de produtos",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Relatório gerado com sucesso"),
+                    @ApiResponse(responseCode = "404", description = "Produto não encontrado")
+            })
     public ResponseEntity<List<RelatorioProdutoDTO>> gerarRelatorioProdutos(
             @RequestParam(required = false) LocalDateTime dataInicio,
             @RequestParam(required = false) LocalDateTime dataFim) {
